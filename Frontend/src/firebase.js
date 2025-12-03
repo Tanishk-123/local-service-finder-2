229bc6a57b233152c928388e3a421a8b202671e2
Tanishk-123 <tanishksethiya@gmail.com>
2025-12-04 00:02:24 +0530

    Removed keys from firebase project

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore, serverTimestamp } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzxb-O-pX8-UOgxmKs2ORlYPhXti_sAYo",
-  authDomain: "local-service-2.firebaseapp.com",
-  projectId: "local-service-2",
-  storageBucket: "local-service-2.firebasestorage.app",
-  messagingSenderId: "905166062258",
-  appId: "1:905166062258:web:5fa8b6071ecf30821bbfab",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",     // enter the credentials here from your web project for client-side 
+  storageBucket: "", // firebase config keys
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
